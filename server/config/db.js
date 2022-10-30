2f5c3799dd9d1cdc0a61cf2e4dc1cb59213ee220
Marco Garcia <marco.paulo.garcia01@gmail.com>
2022-10-30 16:59:16 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI":"mongodb+srv://Admin:Gu9WbKgNrVQSYSWE@cluster0.lwvw7th.mongodb.net/books_store?retryWrites=true&w=majority"
+  "URI":"mongodb+srv://Admin:OZ3t3ksVgXfLhlRw@cluster0.m7cjzeg.mongodb.net/test?retryWrites=true&w=majority"
+};
